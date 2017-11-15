53da5fd5e07928742963ee6e748dc2df13d93ac4
mmumby <meredith.mumby@outlook.com>
2017-11-15 13:19:36 -0800

    added redux state to render a simple book list

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import BookList from '../containers/BookList';
+import '../styles/App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BookList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'JavaScript: The Good Parts'},
+    {title: 'Eloquent JavaScript'},
+    {title: 'Steve Jobs'},
+    {title: 'Elon Musk'}
+  ]
+}
